81ebc4651ac5d75767880ee7eb544402141d9fcf
Viktoshaf <viktoshaf@yandex.ru>
2021-09-04 12:23:20 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+"use string"
+
+/*let num = 5;
+const leftBorderWidth = 1;
+
+
+let numb = 10;
+let arr = [ 5, 3 , "hey"];
+let bool = true;
+ let obj = {
+     name: "Ann",
+     age: 19,
+     job: "manager"
+ };
+
+
+ console.log(obj.job);
+ alert(obj.name + " " + obj.job + " " +  numb);
+ console.log(arr);*/
+
+ //const result = confirm('R U gay?');
+ //console.log(result);
+
+//const okay = confirm("R U Okay?");
+//console.log(okay);
+
+//const okay = prompt("R U Okay?");
+//console.log(okay,'');
+ 
+
+/*const okay = [];
+
+okay[0] = prompt('How r u?','');
+okay[1] = confirm('R U GAY?');
+okay[2] = "Congrats! U're gay and u'r okay";
+
+document.write(okay[2]);
+console.log(okay[0],okay[1]);*/
+
+
+let name = 'Cat';
+
+alert(`Hey, ${name}`);
