d51ae14d70497ae6ffaf6682cf4857cfc6fd5757
Viktoshaf <viktoshaf@yandex.ru>
2021-09-04 20:31:14 +0300

    The first programm

--- js/script.js
+++ js/script.js
@@ -1,43 +1,21 @@
-"use string"
+'use strict';
 
-/*let num = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-let numb = 10;
-let arr = [ 5, 3 , "hey"];
-let bool = true;
- let obj = {
-     name: "Ann",
-     age: 19,
-     job: "manager"
- };
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
- console.log(obj.job);
- alert(obj.name + " " + obj.job + " " +  numb);
- console.log(arr);*/
-
- //const result = confirm('R U gay?');
- //console.log(result);
-
-//const okay = confirm("R U Okay?");
-//console.log(okay);
-
-//const okay = prompt("R U Okay?");
-//console.log(okay,'');
- 
-
-/*const okay = [];
-
-okay[0] = prompt('How r u?','');
-okay[1] = confirm('R U GAY?');
-okay[2] = "Congrats! U're gay and u'r okay";
-
-document.write(okay[2]);
-console.log(okay[0],okay[1]);*/
-
-
-let name = 'Cat';
-
-alert(`Hey, ${name}`);
+console.log(personalMovieDB);
